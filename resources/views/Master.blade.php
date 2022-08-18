7a1d34cb00b2698d120e959d64a7d5347c44efb6
Asim <AsimAli@gmail.com>
2022-08-18 21:37:01 +0500

    added login button

--- resources/views/Master.blade.php
+++ resources/views/Master.blade.php
@@ -11,6 +11,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <br>
+    <button class="btn btn-primary">Login</button>
 </body>
 </html>
